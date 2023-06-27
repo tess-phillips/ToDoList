b63c768a2ecc8080c84a894c45b7b374420d26f9
tess-phillips <124443397+tess-phillips@users.noreply.github.com>
2023-06-27 20:30:03 +0100

    working with edit

--- script.js
+++ script.js
@@ -1,48 +1,53 @@
-import { createElement } from "./createElements.js"
+import { createElement } from "./createElements.js";
 
-document.querySelector('#push').onclick = function(){
-    if(document.querySelector('#newtask input').value.length == 0){
-        alert("Please Enter a Task")
-    }
-    else{
-        const tasksBlock = document.querySelector('#tasks');
-        const taskValue = document.querySelector('#newtask input').value;
-        
-        const div = createElement("div", { className: "task" });
-        const checkbox = createElement("input", { type: "checkbox", id: "box1", name: "box1" });
-        const label = createElement("label", { id: "taskname", for: "box1", innerHTML: taskValue });
-        const deleteBtn = createElement("button", { className: "delete", innerHTML: "delete" });
-        const editBtn = createElement("button", { className: "edit", onclick: editTask, innerHTML: "edit" });
-        // label.append(editBtn)
-        div.append(checkbox, label, deleteBtn, editBtn)
-        tasksBlock.append(div);
-        
+document.querySelector('#push').onclick = function () {
+  if (document.querySelector('#newtask input').value.length == 0) {
+    alert("Please Enter a Task");
+  } else {
+    const tasksBlock = document.querySelector('#tasks');
+    const taskValue = document.querySelector('#newtask input').value;
 
-        // document.querySelector('#tasks').innerHTML += `
-        //     <div class="task">
-        //         <label ype="checkbox" id="taskname" name="box1">
-        //             ${document.querySelector('#newtask input').value} //for box1
-        //         </label>
-        //         <button class="delete">
-        //             <i class="far fa-trash-alt"></i>
-        //         </button>
-        //     </div>
-        // `;
+    const div = createElement("div", { className: "task" });
+    const checkbox = createElement("input", { type: "checkbox", id: "box1", name: "box1" });
+    const label = createElement("label", { id: "taskname", htmlFor: "box1", innerHTML: taskValue });
+    const deleteBtn = createElement("button", { className: "delete", innerHTML: "delete" });
+    const editBtn = createElement("button", { className: "edit", innerHTML: "edit" });
+    div.append(deleteBtn, editBtn,checkbox, label);
+    tasksBlock.append(div);
 
-        var current_tasks = document.querySelectorAll(".delete");
-        for(var i=0; i<current_tasks.length; i++){
-            current_tasks[i].onclick = function(){
-                this.parentNode.remove();
-            }
-        }
-            
-        var tasks = document.querySelectorAll(".task");
-        for(var i=0; i<tasks.length; i++){
-            tasks[i].onclick = function(){
-                this.classList.toggle('completed');
-            }
-        }
+    deleteBtn.onclick = function () {
+      this.parentNode.remove();
+    };
 
-        document.querySelector("#newtask input").value = "";
-    }
-}
+    div.onclick = function () {
+      this.classList.toggle('completed');
+    };
+
+    editBtn.onclick = function () {
+      const taskText = label.textContent;
+      const editInput = createElement("input", { type: "text", value: taskText });
+      const saveBtn = createElement("button", { innerHTML: "Save" });
+      const cancelButton = createElement("button", { innerHTML: "Cancel" });
+
+      div.replaceChild(editInput, label);
+      div.append(saveBtn, cancelButton);
+
+      saveBtn.onclick = function () {
+        label.textContent = editInput.value;
+        div.removeChild(editInput);
+        div.removeChild(saveBtn);
+        div.removeChild(cancelButton);
+        div.insertBefore(label, div.secondChild); // Insert label as the first child of div
+      };
+      
+      cancelButton.onclick = function () {
+        div.removeChild(editInput);
+        div.removeChild(saveBtn);
+        div.removeChild(cancelButton);
+        div.insertBefore(label, div.secondChild); // Insert label as the first child of div
+      };
+    };
+
+    document.querySelector("#newtask input").value = "";
+  }
+};
