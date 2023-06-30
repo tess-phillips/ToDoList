b974ecdc4474970a488c13d7ba15b2c7ed19c08a
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-06-30 16:56:36 +0100

    Create template

--- helpers/createElements.js
+++ helpers/createElements.js
@@ -1,17 +1,19 @@
 /**
- * creates DOM elements 
+ * creates DOM elements
  * e.g. createElement('p',{id: "para3", innerHTML: "slay"})
  *  ^ creates: <p id="para3">slay</p>
- * @param {*} tagName 
+ * @param {*} tagName
  * @param {*} attributes {attribute: the_attribute}
- * @returns 
+ * @returns
  */
 export const createElement = (tagName, attributes = {}) => {
   const element = document.createElement(tagName);
-
+  console.log("attributes ", attributes);
+  // let count = 1;
   for (const attribute in attributes) {
+    console.log("attribute: " + attribute);
     element[attribute] = attributes[attribute];
+    // count++;
   }
-
   return element;
 };
