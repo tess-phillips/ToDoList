d498dc0f7839a18ecae5548f7aff1e7dc28621ff
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-07-04 11:08:48 +0100

    editTask added

--- helpers/editDelete.js
+++ helpers/editDelete.js
@@ -4,45 +4,33 @@ export function handleBtnClick(btn) {
   switch (btn.id) {
     case "deleteBtn":
       deleteTask(btn);
+    case "editBtn":
+      editTask(btn);
   }
 }
 // removes the task
-export function deleteTask(btn) {
-  const item = btn.parentNode.parentNode;
-  console.log(item);
+function deleteTask(task) {
+  const item = task.parentNode.parentNode;
   item.remove();
 }
 
 // edits the task
-export function editTask(task) {
-  // gets the text that is already on the task
-  const span = task.querySelector("span");
-  const editInput = task.querySelector("#editInput");
-  const taskText = span.textContent;
-  console.log(taskText);
-  // creates the input and new buttons
-  // const editInput = createElement("input", { type: "text", value: taskText });
-  // const saveBtn = createElement("button", { innerHTML: "Save" });
-  // const cancelButton = createElement("button", { innerHTML: "Cancel" });
+function editTask(task) {
+  // get parent divs
+  const parentDiv = task.parentNode;
+  const taskDiv = parentDiv.parentNode;
 
-  // removes the existing text with the input (containing the prev task), save and cancel buttons
-  task.replaceChild(editInput, span);
-  // div.append(saveBtn, cancelButton);
+  // grab text that needs to be edited and place it inside the (edit)input
+  const span = taskDiv.querySelector("#toDo");
+  let text = span.textContent;
 
-  saveBtn.onclick = function () {
-    label.textContent = editInput.value;
-    div.removeChild(editInput);
-    div.removeChild(saveBtn);
-    div.removeChild(cancelButton);
-    div.insertBefore(label, div.secondChild);
-  };
+  const editInput = taskDiv.querySelector("#editInput");
+  editInput.value = text;
 
-  cancelButton.onclick = function () {
-    div.removeChild(editInput);
-    div.removeChild(saveBtn);
-    div.removeChild(cancelButton);
-    div.insertBefore(label, div.secondChild);
-  };
+  // Show/hide parent divs
+  toggleVisibility(parentDiv);
+  const editTaskDiv = taskDiv.querySelector(".editTaskDiv");
+  toggleVisibility(editTaskDiv);
 }
 
 function toggleVisibility(div) {
