ca4e08602b6f7b0b29cca51d0f41c87e15d12074
Tommaso Orlandi <tommaso.orlandi3@gmail.com>
2023-07-06 19:17:12 +0100

    same IDs issue fixed and added a unique ID to each task

--- helpers/createElements.js
+++ helpers/createElements.js
@@ -1,19 +1,28 @@
 const tasksBlock = document.querySelector("#tasksContainer");
 
+let taskIdCounter = 1; // Counter variable to keep track of the current ID
+
 export function createTask(task) {
   // get template for tasks (from html)
   const template = document.querySelector("#taskTemplate");
   // clone the content of the template (returns a DocumentFragment)
   const domFragment = template.content.cloneNode(true);
+
+  const taskId = `task-${taskIdCounter}`; // Generate unique ID
+  domFragment.querySelector("li").setAttribute("id", taskId);
+
   // add content into the span (<li> element)
   domFragment.querySelector("#toDo").textContent = task;
 
   //adds date to task
   const currentDate = new Date();
-  domFragment.querySelector("#task").setAttribute("data-date", currentDate.toISOString())
+  domFragment
+    .querySelector("li")
+    .setAttribute("data-date", currentDate.toISOString());
   tasksBlock.appendChild(domFragment);
-  
-  const newTask = document.querySelector("#task");
+
+  taskIdCounter++; // Increment the counter for the next task
+
+  const newTask = document.querySelector("li");
   return newTask;
 }
-
